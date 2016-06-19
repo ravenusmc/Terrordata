f3ad1296aaecc6cfe71427867f2bfb838e2d793c
ravenusmc <mcuddy77@gmail.com>
2016-06-19 17:38:22 -0400

    added an ability to select which country tolook at.

--- terror.py
+++ terror.py
@@ -3,10 +3,24 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
+print("Here are the county codes:")
+print("1. Belgium")
+print("2. Denmark")
+print("3. France")
+print("4. Germany")
+print("5. Greece")
+print("6. Ireland")
+print("7. Italy")
+print("8. Luxembourg")
+print("9. Netherlands")
+print("10. Portugal")
+print("11. Spain")
+print("12. United Kingdom")
+
 #Saving the csv file as filename.
 filename = 'europe_terrorist_data.csv'
 
-turning the filenam over to an object. 
+#turning the filenam over to an object. 
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
@@ -14,26 +28,32 @@ with open(filename) as f:
 
   #building arrays to contain the data. 
   dates, countries, countries2, countries3 = [], [], [], []
+
+  value = int(input("Please enter in a value: "))
+
   #A for loop that will cycle through the CSV file pullign the specific rows of data. 
   for row in reader:
     year = datetime.strptime(row[0], "%Y")
     dates.append(year)
 
-    country = int(row[3])
+
+    country = int(row[value])
     countries.append(country)
 
-    country2 = int(row[4])
-    countries2.append(country2)
+    # country2 = int(row[4])
+    # countries2.append(country2)
 
-    country3 = int(row[12])
-    countries3.append(country3)
+    # country3 = int(row[5])
+    # countries3.append(country3)
+
 
 #Using matplotlib to graphically represent the data. 
 fig = plt.figure(dpi=128, figsize=(10,6))
 
 plt.plot(dates, countries, c="blue")
-plt.plot(dates, countries2, c="red")
-plt.plot(dates, countries3, c="orange")
+#
+# plt.plot(dates, countries2, c="red")
+# plt.plot(dates, countries3, c="orange")
 plt.title("Terrorist Attacks in Europe, 1970 to Present", fontsize=24)
 plt.xlabel("year", fontsize=16)
 
