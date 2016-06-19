87ddd870724c2103efe425f051d11f9dee982ce3
ravenusmc <mcuddy77@gmail.com>
2016-06-19 17:25:01 -0400

    added comments to the terror.py file

--- terror.py
+++ terror.py
@@ -3,14 +3,18 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
+#Saving the csv file as filename.
 filename = 'europe_terrorist_data.csv'
 
+turning the filenam over to an object. 
 with open(filename) as f:
   reader = csv.reader(f)
   header_row = next(reader)
   #print(header_row)
 
+  #building arrays to contain the data. 
   dates, countries, countries2, countries3 = [], [], [], []
+  #A for loop that will cycle through the CSV file pullign the specific rows of data. 
   for row in reader:
     year = datetime.strptime(row[0], "%Y")
     dates.append(year)
@@ -24,18 +28,19 @@ with open(filename) as f:
     country3 = int(row[12])
     countries3.append(country3)
 
+#Using matplotlib to graphically represent the data. 
 fig = plt.figure(dpi=128, figsize=(10,6))
+
 plt.plot(dates, countries, c="blue")
 plt.plot(dates, countries2, c="red")
 plt.plot(dates, countries3, c="orange")
-
 plt.title("Terrorist Attacks in Europe, 1970 to Present", fontsize=24)
 plt.xlabel("year", fontsize=16)
 
 fig.autofmt_xdate()
 
 plt.ylabel("Number of attacks", fontsize=16)
-
+#The .show() function is what will execute and pull the data to graphically represent it. 
 plt.show()
 
 
