0889b3d0e8e74ba897ea9333ef631f08f8bed004
ravenusmc <mcuddy77@gmail.com>
2016-06-19 17:47:38 -0400

    added more functions to the program to help to break up the code.

--- terror.py
+++ terror.py
@@ -3,64 +3,83 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-print("Here are the county codes:")
-print("1. Belgium")
-print("2. Denmark")
-print("3. France")
-print("4. Germany")
-print("5. Greece")
-print("6. Ireland")
-print("7. Italy")
-print("8. Luxembourg")
-print("9. Netherlands")
-print("10. Portugal")
-print("11. Spain")
-print("12. United Kingdom")
 
-#Saving the csv file as filename.
-filename = 'europe_terrorist_data.csv'
+def welcome():
+  print("----------------------------------------")
+  print("Welcome to Terrorist attack data plotter")
+  print("This project will show terrorist attacks")
+  print("The user will be asked to select a country")
+  print("and then the terrorist attacks will be shown from 1970")
+  print("----------------------------------------")
 
-#turning the filenam over to an object. 
-with open(filename) as f:
-  reader = csv.reader(f)
-  header_row = next(reader)
-  #print(header_row)
+def country():
+  print("Here are the county codes:")
+  print("1. Belgium")
+  print("2. Denmark")
+  print("3. France")
+  print("4. Germany")
+  print("5. Greece")
+  print("6. Ireland")
+  print("7. Italy")
+  print("8. Luxembourg")
+  print("9. Netherlands")
+  print("10. Portugal")
+  print("11. Spain")
+  print("12. United Kingdom")
 
-  #building arrays to contain the data. 
-  dates, countries, countries2, countries3 = [], [], [], []
+def data():
 
-  value = int(input("Please enter in a value: "))
+  #Saving the csv file as filename.
+  filename = 'europe_terrorist_data.csv'
 
-  #A for loop that will cycle through the CSV file pullign the specific rows of data. 
-  for row in reader:
-    year = datetime.strptime(row[0], "%Y")
-    dates.append(year)
+  #turning the filenam over to an object. 
+  with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    #print(header_row)
+
+    #building arrays to contain the data. 
+    dates, countries, countries2, countries3 = [], [], [], []
+
+    value = int(input("Please enter in a value: "))
+
+    #A for loop that will cycle through the CSV file pullign the specific rows of data. 
+    for row in reader:
+      year = datetime.strptime(row[0], "%Y")
+      dates.append(year)
+
+      country = int(row[value])
+      countries.append(country)
+
+      # country2 = int(row[4])
+      # countries2.append(country2)
+
+      # country3 = int(row[5])
+      # countries3.append(country3)
 
 
-    country = int(row[value])
-    countries.append(country)
+  #Using matplotlib to graphically represent the data. 
+  fig = plt.figure(dpi=128, figsize=(10,6))
 
-    # country2 = int(row[4])
-    # countries2.append(country2)
+  plt.plot(dates, countries, c="blue")
+  #
+  # plt.plot(dates, countries2, c="red")
+  # plt.plot(dates, countries3, c="orange")
+  plt.title("Terrorist Attacks in Europe, 1970 to Present", fontsize=24)
+  plt.xlabel("year", fontsize=16)
 
-    # country3 = int(row[5])
-    # countries3.append(country3)
+  fig.autofmt_xdate()
 
+  plt.ylabel("Number of attacks", fontsize=16)
+  #The .show() function is what will execute and pull the data to graphically represent it. 
+  plt.show()
 
-#Using matplotlib to graphically represent the data. 
-fig = plt.figure(dpi=128, figsize=(10,6))
+def main():
+  welcome()
+  country()
+  data()
 
-plt.plot(dates, countries, c="blue")
-#
-# plt.plot(dates, countries2, c="red")
-# plt.plot(dates, countries3, c="orange")
-plt.title("Terrorist Attacks in Europe, 1970 to Present", fontsize=24)
-plt.xlabel("year", fontsize=16)
+main()
 
-fig.autofmt_xdate()
-
-plt.ylabel("Number of attacks", fontsize=16)
-#The .show() function is what will execute and pull the data to graphically represent it. 
-plt.show()
 
 
